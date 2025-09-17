7115904b4581b60a3e8bf8d26b52a5158a595e8e
mindlessJoe <manuel.dinnocenti@intuos.it>
2025-09-17 09:03:49 +0200

    Primo committ fu<ionante (si spera)

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mindlessjoe_mips_core (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,14 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+assign uio_oe = 8'hFF;
+top_module SoC(
+    
+    .clk(clk),
+    .rst(rst_n),
+    .data_in(ui_in),
+    .data_out(uo_out),
+    .address_out(uio_out)
+);
 
 endmodule
